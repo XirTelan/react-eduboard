f39c418bb5b7f0c183a812971261cfc4a3dbbe73
XirTelan <prssergey@gmail.com>
2022-11-03 13:25:29 +0300

    small fix, add icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,15 @@
 import { Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import NavListItem from './UI/NavListItem';
-
+import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
+import EventAvailableIcon from '@mui/icons-material/EventAvailable';
+import EventRepeatIcon from '@mui/icons-material/EventRepeat';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import GroupsIcon from '@mui/icons-material/Groups';
+import TodayIcon from '@mui/icons-material/Today';
+import EventIcon from '@mui/icons-material/Event';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import TocIcon from '@mui/icons-material/Toc';
 export default function Sidebar() {
   return (
     <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-white ">
@@ -15,16 +23,32 @@ export default function Sidebar() {
         <ul
           className="nav nav-pills w-100 flex-column mb-sm-auto mb-0 align-text-center align-items-center align-items-sm-start"
           id="menu">
-          <Typography variant='subtitle1'>Основное</Typography>
-          <NavListItem to="/attendance" title="Посещяемость" />
-          <NavListItem to="/current-controll" title="Текущий контроль" />
-          <NavListItem to="/intersessional-controll" title="Межсессионный контроль" />
-          <NavListItem to="/intermediate-controll" title="Промежуточная аттестация" />
-          <NavListItem to="/entrance-controll" title="Входной контроль" />
+          <Typography variant="subtitle1">Основное</Typography>
+          <NavListItem to="/attendance" title="Посещяемость">
+            <CalendarMonthIcon />
+          </NavListItem>
+          <NavListItem to="/current-controll" title="Текущий контроль">
+            <EventRepeatIcon />
+          </NavListItem>
+          <NavListItem to="/intersessional-controll" title="Межсессионный контроль">
+            <TodayIcon />
+          </NavListItem>
+          <NavListItem to="/intermediate-controll" title="Промежуточная аттестация">
+            <EventIcon />
+          </NavListItem>
+          <NavListItem to="/entrance-controll" title="Входной контроль">
+            <EventAvailableIcon />
+          </NavListItem>
           <label>Настройки</label>
-          <NavListItem to="/groups" title="Группы" />
-          <NavListItem to="/specialities" title="Специальности" />
-          <NavListItem to="/discipline" title="Дисциплины" />
+          <NavListItem to="/groups" title="Группы">
+            <GroupsIcon />
+          </NavListItem>
+          <NavListItem to="/specialities" title="Специальности">
+            <ListAltIcon />
+          </NavListItem>
+          <NavListItem to="/discipline" title="Дисциплины">
+            <TocIcon />
+          </NavListItem>
           <hr className="w-100 text-secondary" />
         </ul>
       </div>
--- src/components/UI/NavListItem.tsx
+++ src/components/UI/NavListItem.tsx
@@ -3,14 +3,15 @@ import { Link } from "react-router-dom";
 
 interface NavListItemProps {
     to: string,
-    title: string
+    title: string,
+    children?: React.ReactNode
 }
 
-export default function NavListItem({to,title}:NavListItemProps ) {
+export default function NavListItem({to,title,children}:NavListItemProps ) {
   return (
     <li className="nav-item w-100">
       <NavLink to={to} className="nav-link  px-0 align-middle">
-        <span className="m-3 d-none d-sm-inline">{title}</span>
+        {children} <span className="me-3 d-none d-sm-inline">{title}</span>
       </NavLink>
     </li>
   )
