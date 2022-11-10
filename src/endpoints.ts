ba32707bdb7b0ea191204806f006decb83b6e285
XirTelan <prssergey@gmail.com>
2022-11-10 16:50:25 +0300

    0.3.6 added axios

---
+++ src/endpoints.ts
@@ -0,0 +1,3 @@
+const baseURL = process.env.REACT_APP_API_URL;
+console.log(process.env.REACT_APP_API_URL);
+export const urlSpecialities = `${baseURL}/specialities`;
--- src/page/SpecialitiesList.tsx
+++ src/page/SpecialitiesList.tsx
@@ -1,11 +1,20 @@
+import axios, { AxiosResponse } from 'axios';
 import { Box, Button, Typography } from '@mui/material';
 
 import CollapseListItem from '../components/UI/CollapseListItem';
 import Header from '../components/UI/Header';
 import { disciplines, specialities } from '../data/data';
 import { disciplineDTO, specialityDTO } from '../types';
+import { useEffect } from 'react';
+import { urlSpecialities } from '../endpoints';
 
 export default function SpecialitiesList() {
+  useEffect(() => {
+    axios.get(urlSpecialities).then((responce: AxiosResponse<specialityDTO[]>) => {
+      console.log(responce.data);
+    });
+  }, []);
+
   return (
     <Box
       maxHeight="89vh"
